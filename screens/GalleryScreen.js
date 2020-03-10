adab8c6a19cfcc807018bd72f234aed5cd2163e6
Trevor Thompson <Trevor.Ray.Thompson@gmail.com>
2020-03-10 15:41:20 -0700

    flastList working

--- screens/GalleryScreen.js
+++ screens/GalleryScreen.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { FlatList, Image, StyleSheet, Text, View } from 'react-native';
+import { FlatList, Image, StyleSheet, Text, View, SafeAreaView } from 'react-native';
 import { AppContext } from '../context/appContext';
 
 export default function Gallery() {
@@ -9,40 +9,28 @@ export default function Gallery() {
   console.log('gallery', gallery[0].image.localUri);
 
   return (
-      <View>
-          <Image
-          style={{width: 100, height: 200}}
-          source={{uri: gallery[0].image.localUri}}
-          />
-
-        {/* <Text>Hello</Text> */}
+      <SafeAreaView style={styles.container}>
         <FlatList
-          style={styles.flatlist}
           data={appContext.gallery}
-          renderItem={({item}) => {
-            console.log('image', item.image.localUri);
-              return (
-              <Image style={styles.images} source={{uri: item.image.localUri}}/>
-              )
-          }}
-          keyExtractor={item => item.id}
+          renderItem={({item}) => <Image style={styles.images} source={{uri: item.image.uri}}/>}
+          keyExtractor={item => item.id}  
           ListEmptyComponent={<Text>Nothing to Display</Text>}
           />
-      </View>
-
+      </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  flatlist: {
+  container: {
     flex: 1,
-    borderColor: 'red',
-    borderWidth: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
   },
   images: {
-    width: 100, 
+    width: 200, 
     height: 200, 
     borderColor: 'green', 
     borderWidth: 1,
+    marginBottom: 10
   }
 });
